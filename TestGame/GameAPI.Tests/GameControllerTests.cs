e7d89b5a81028c19d3f14bbf57fd7335c0ff6837
TedH04 <105521543+TedH04@users.noreply.github.com>
2023-10-20 15:04:00 +0200

    Test för api

---
+++ TestGame/GameAPI.Tests/GameControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace GameAPI.Tests
+{
+    public class GameControllerTests
+    {
+        #region Constructor
+        private readonly ITestOutputHelper _log;
+
+        public GameControllerTests(ITestOutputHelper output)
+        {
+            _log = output;
+        }
+        #endregion
+
+        [Fact]
+        public async Task GetGameState_ReturnsOk_WithGameState()
+        {
+            //Arrange
+            using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/game/state");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            //Log
+            _log.WriteLine($"Response HTTP Status Code: {response.StatusCode}");
+            var content = await response.Content.ReadAsStringAsync();
+            _log.WriteLine($"Response Content: {content}");
+        }
+    }
+}
