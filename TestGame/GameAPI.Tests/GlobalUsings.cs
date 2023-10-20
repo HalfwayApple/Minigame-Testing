3ac6456c04583cb1ce4dc76e28516ebaefb94f1d
TedH04 <105521543+TedH04@users.noreply.github.com>
2023-10-20 14:04:48 +0200

    Character test tillagd för att testa attackera en fiende, kollar om armor/dmg och mock fiender funkar, installerat Moq för dessa tester.

--- TestGame/GameAPI.Tests/GlobalUsings.cs
+++ TestGame/GameAPI.Tests/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using GameAPI.Data.Characters;
 global using GameAPI.Data.Items.Equipment.Armors;
 global using GameAPI.Data.Items.Equipment.Weapons;
+global using Moq;
 global using Xunit;
 global using Xunit.Abstractions;
