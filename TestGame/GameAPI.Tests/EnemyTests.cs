90bd3b03f0d236da3021253d1546cb6ec84e205a
TedH04 <105521543+TedH04@users.noreply.github.com>
2023-10-20 14:19:17 +0200

    enemy tests klar, testar om droptable är tomt samt om den droppar en infantry lasgun, testar även metoder CalcNormalDamage så den funkar med fiendes damage.

---
+++ TestGame/GameAPI.Tests/EnemyTests.cs
@@ -0,0 +1,69 @@
+﻿using GameAPI.Data.Items.Equipment;
+
+namespace GameAPI.Tests
+{
+    public class EnemyTests
+    {
+        #region Constructor
+        private readonly ITestOutputHelper _log;
+
+        public EnemyTests(ITestOutputHelper output)
+        {
+            _log = output;
+        }
+        #endregion
+
+        [Fact]
+        public void CalcNormalDamage_ReturnsAttackPowerValue()
+        {
+            // Arrange
+            var enemy = new Enemy();
+            enemy.AttackPower = 25;
+
+            // Act
+            int damage = enemy.CalcNormalDamage();
+
+            //Log
+            _log.WriteLine($"fiendes attackpower: {damage}");
+
+            // Assert
+            Assert.Equal(25, damage);
+        }
+
+        [Fact]
+        public void DropEquipment_ReturnsNull_WhenLootTableIsEmpty()
+        {
+            // Arrange
+            var enemy = new Enemy();
+            enemy.LootTable = new List<Equipment>();
+
+            // Act
+            var equipment = enemy.DropEquipment();
+
+            //Log
+            _log.WriteLine($"dina drops finns inte");
+
+            // Assert
+            Assert.Null(equipment);
+        }
+
+        [Fact]
+        public void DropEquipment_ReturnsEquipmentFromLootTable()
+        {
+            // Arrange
+            var enemy = new Enemy();
+            var mockEquipment = new Mock<Equipment>().Object;
+            mockEquipment.Name = "infantry lasgun";
+            enemy.LootTable.Add(mockEquipment);
+
+            // Act
+            var droppedEquipment = enemy.DropEquipment();
+
+            //Log
+            _log.WriteLine($"Dropped equipment: {droppedEquipment.Name}");
+
+            // Assert
+            Assert.Contains(droppedEquipment, enemy.LootTable);
+        }
+    }
+}
