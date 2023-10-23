3b4fa80682660209443aec730db14aa523aa9dde
AleDotz <114862977+AleDotz@users.noreply.github.com>
2023-10-23 12:06:54 +0200

    Push to main

---
+++ src/components/views/PlayGame.jsx
@@ -0,0 +1,81 @@
+import "./PlayGame.css";
+import { React, useContext, useState } from "react";
+import { GameContext } from "../../contexts/GameContext";
+
+const PlayGame = () => {
+
+    const {currentGameState, setCurrentGameState, enterBattle, attackEnemy} = useContext(GameContext);
+
+    let enemyImage = "";
+
+    if(currentGameState.location.name != "Town"){
+        if (currentGameState.location.enemy.name === "Slime"){
+            enemyImage = "https://cdn.discordapp.com/attachments/1024651946721824768/1164949923725316096/slimepicfixxed.png?ex=65451329&is=65329e29&hm=e29ccf5814c4ff09a6d1ecf2285877fc2b8036f8d41062fe6b9e0a2e2053a0bd&"
+        }else if(currentGameState.location.enemy.name === "Rat"){
+            enemyImage = "https://i.pinimg.com/originals/2f/7c/78/2f7c78c4378b7aae8d237e083732884f.png"
+        }
+    }
+    
+
+    console.log("bajs");
+    console.log(currentGameState);
+
+  return (
+    <div className="game-container">
+        {currentGameState ? (
+            <>
+                <div className="hero-card">
+                    <img className="fighterImage" src="https://cdn.discordapp.com/attachments/1068620070227562659/1164550263479226449/imageedit_4_5377634979.gif?ex=65439ef3&is=653129f3&hm=0a989bec8fefb11f7c983f22dff0c21443ac03f6a8a5a4ca7d105d99c034b2d6&" alt="Hero" />
+                    <h3>{currentGameState.hero.name} The Hero</h3>
+                    <p>Level: {currentGameState.hero.level}</p>
+                    <p>HP: {currentGameState.hero.currentHP}/{currentGameState.hero.maxHP}</p>
+                    <p>Mana: {currentGameState.hero.currentMana}/{currentGameState.hero.maxMana}</p>
+                    {currentGameState.hero.equippedWeapon ? (
+                        <p>Weapon: {currentGameState.hero.equippedWeapon.name} + {currentGameState.hero.equippedWeapon.attackPower}</p>
+                    ) : (
+                        <p>Weapon: Unarmed</p>
+                    )}
+                    <p>Armor: {currentGameState.hero.armorValue}</p>
+                    <p>Attack Power: {currentGameState.hero.attackPower}</p>
+                    <p>Experience: {currentGameState.hero.xp}</p>
+                </div>
+            
+                <div>
+                    <ul className="action-flow">
+                        <li>You are currently in: {currentGameState.location.name}</li>
+                    </ul>
+                    <div className="button-container">
+                        {currentGameState.location.name != "Battle" &&
+                            <button className="game-button" onClick={() => enterBattle()}>Challenge an enemy</button>
+                        }
+                        {currentGameState.location.name == "Battle" &&
+                            <button className="game-button" onClick={() => attackEnemy()}>Attack</button>
+                        }
+                        <button className="game-button">Continue</button>
+                    </div>
+                </div>
+                
+                {currentGameState.location.name != "Town" &&
+                    <div className="enemy-card">
+                        <img className="fighterImage" src={enemyImage} alt="Enemy" />
+                        <h3>{currentGameState.location.enemy.name}</h3>
+                        <p>Level: {currentGameState.location.enemy.level}</p>
+                        <p>HP: {currentGameState.location.enemy.currentHP}/{currentGameState.location.enemy.maxHP}</p>
+                        <p>Mana: {currentGameState.location.enemy.currentMana}/{currentGameState.location.enemy.maxMana}</p>
+                        <p>Armor: {currentGameState.location.enemy.armorValue}</p>
+                        <p>Attack: {currentGameState.location.enemy.attackPower}</p>
+                    </div>
+                }
+                
+                
+            </>
+            
+        ) : (
+            <p>Error Loading game</p>
+        )}
+      
+    </div>
+  )
+}
+
+export default PlayGame
