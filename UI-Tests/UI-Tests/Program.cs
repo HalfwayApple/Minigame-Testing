3f293d03047f4087dc36cdf3c88297c0fa52b593
TedH04 <105521543+TedH04@users.noreply.github.com>
2023-10-23 13:02:37 +0200

    ui-tester med playwright bas skapad med lite placeholders bara, måste lägga till vad den ska testa sen.

---
+++ UI-Tests/UI-Tests/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Playwright;
+
+namespace UI_Tests
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // startar playwright
+            using var playwright = await Playwright.CreateAsync();
+
+            // gör detta för att kunna köra playwright med microsoft edge
+            var edgeExecutablePath = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";  // This is a common default path, adjust if necessary
+
+            await using var browser = await playwright.Chromium.LaunchAsync(new()
+            {
+                Headless = true,
+                ExecutablePath = edgeExecutablePath
+            });
+
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://localhost:placeholder/");
+
+            var placeHolder = await page.QuerySelectorAllAsync("med den här får man bara ut inre text från id på hemsidan");
+
+            var placeHolderForPageText = placeHolder[0].InnerTextAsync();
+
+
+            Console.WriteLine($"nånting händer kanske skriva vad");
+            Console.ReadLine();
+        }
+    }
+}
