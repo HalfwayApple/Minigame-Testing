9107d17e0b55c53e4c1cb7e66ecca41db38fcb28
TedH04 <105521543+TedH04@users.noreply.github.com>
2023-10-26 15:58:42 +0200

    music och welcomepage

--- Frontend/src/components/views/WelcomePage.jsx
+++ Frontend/src/components/views/WelcomePage.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import './WelcomePage.css';  // Make sure to import the CSS file
 
 const WelcomePage = () => {
     const navigate = useNavigate()
 
     const handleClick = () => {
-        navigate('/PlayGame')
+        // Adding a timeout to create a delay for the transition effect
+        setTimeout(() => {
+            navigate('/PlayGame');
+        }, 500);
     }
 
-  return (
-    <div>
-      <button onClick={() => handleClick()}>Start game</button>
-    </div>
-  )
+    return (
+        <div className="welcome-container">
+            <button className="button" onClick={() => handleClick()}>Start Game</button>
+        </div>
+    )
 }
 
 export default WelcomePage
