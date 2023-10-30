a5e0a97e79afb1df3be0088ccf115c7dc1628c55
AleDotz <114862977+AleDotz@users.noreply.github.com>
2023-10-30 13:13:33 +0100

    Add more controller tests for BadRequests

--- TestGame/GameAPI.Tests/GameControllerTests.cs
+++ TestGame/GameAPI.Tests/GameControllerTests.cs
@@ -115,6 +115,24 @@
         }
 
         [Fact]
+        public async Task Battle_ReturnsBadRequest()
+        {
+            //Arrange
+            using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/game/battle");
+            response.StatusCode = HttpStatusCode.BadRequest;
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            //Log
+            LogResponse(response);
+        }
+
+        [Fact]
         public async Task Attack_ReturnsOk_WithGameState()
         {
             //Arrange
@@ -129,6 +147,28 @@
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            _log.WriteLine($"status kod: {response.StatusCode}");
+
+            // Log
+            LogResponse(response);
+        }
+
+        [Fact]
+        public async Task Attack_ReturnsBadRequest()
+        {
+            //Arrange
+            using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            // startar en Battle (måste göras för att Attack ska fungera)
+            await client.GetAsync("/game/battle");
+
+            // Act Attack efter att den startat Battle
+            var response = await client.GetAsync("/game/attack");
+            response.StatusCode = HttpStatusCode.BadRequest;
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
             _log.WriteLine($"status kod: {response.StatusCode}");
 
             // Log
@@ -154,6 +194,25 @@
         }
 
         [Fact]
+        public async Task ReturnToTown_ReturnsBadRequest()
+        {
+            // Arrange
+            using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/game/returnToTown");
+            response.StatusCode = HttpStatusCode.BadRequest;
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            _log.WriteLine($"Status kod: {response.StatusCode}");
+
+            // Log
+            LogResponse(response);
+        }
+
+        [Fact]
         public async Task EnterShop_ReturnsOk_WithGameState()
         {
             // Arrange
@@ -165,6 +224,22 @@
 
             // Assert 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            _log.WriteLine($"Status kod: {response.StatusCode}");
+        }
+
+        [Fact]
+        public async Task EnterShop_ReturnsBadRequest()
+        {
+            // Arrange
+            using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/game/enterStore");
+            response.StatusCode = HttpStatusCode.BadRequest;
+
+            // Assert 
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
             _log.WriteLine($"Status kod: {response.StatusCode}");
         }
 
