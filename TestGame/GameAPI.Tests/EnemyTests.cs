8b05f518e69c78ba97e63a0969a083f284c06bac
TedH04 <105521543+TedH04@users.noreply.github.com>
2023-10-31 11:28:38 +0100

    positiva och negativa tester tillagda & exception tillagd i enemy.cs

--- TestGame/GameAPI.Tests/EnemyTests.cs
+++ TestGame/GameAPI.Tests/EnemyTests.cs
@@ -60,6 +60,33 @@
             Assert.Null(equipment);
         }
 
+        [Fact]
+        public void DropEquipment_ReturnsNull_WhenLootTableIsNull()
+        {
+            // Arrange
+            var enemy = new Enemy();
+            enemy.LootTable = null;
+
+            // Act
+            var droppedEquipment = enemy.DropEquipment();
+
+            // Assert
+            Assert.Null(droppedEquipment);
+        }
+
+        [Theory]
+        [InlineData(-5)]
+        [InlineData(101)]
+        public void DropEquipment_ShouldThrowException_WhenNoDropChanceIsOutOfBounds(int invalidNoDropChance)
+        {
+            // Arrange
+            var enemy = new Enemy();
+            enemy.NoDropChance = invalidNoDropChance;
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => enemy.DropEquipment());
+        }
+
         //Positivt scenario
         [Fact]
         public void DropEquipment_ReturnsEquipmentFromLootTable()
@@ -80,5 +107,66 @@
             // Assert
             Assert.Contains(droppedEquipment, enemy.LootTable);
         }
+
+        [Fact]
+        public void DropEquipment_ReturnsNull_WhenNoDropChanceIs100()
+        {
+            // Arrange
+            var enemy = new Enemy();
+            enemy.NoDropChance = 100;
+            var mockEquipment = new Equipment();
+            mockEquipment.Name = "TestWeapon";
+            enemy.LootTable.Add(mockEquipment);
+
+            // Act
+            var droppedEquipment = enemy.DropEquipment();
+
+            // Assert
+            Assert.Null(droppedEquipment);
+        }
+
+        [Fact]
+        public void DropEquipment_CanReturnAnyItemFromLootTable()
+        {
+            // Arrange
+            var enemy = new Enemy();
+            enemy.NoDropChance = 0;
+            var mockEquipment1 = new Equipment { Name = "Weapon1" };
+            var mockEquipment2 = new Equipment { Name = "Weapon2" };
+            enemy.LootTable.AddRange(new[] { mockEquipment1, mockEquipment2 });
+
+            // Act & Assert
+            var results = new HashSet<string>();
+            for (int i = 0; i < 100; i++)
+            {
+                var droppedEquipment = enemy.DropEquipment();
+                results.Add(droppedEquipment.Name);
+            }
+            Assert.Contains(mockEquipment1.Name, results);
+            Assert.Contains(mockEquipment2.Name, results);
+        }
+
+        [Fact]
+        public void Clone_ReturnsClone_ButNotIdentical()
+        {
+            // Arrange
+            var originalEnemy = new Enemy
+            {
+                Id = 1,
+                Name = "TestEnemy",
+                LootTable = new List<Equipment>
+                {
+                    new Equipment { Name = "Item1" },
+                    new Equipment { Name = "Item2" }
+                }
+            };
+
+            // Act
+            var clonedEnemy = (Enemy)originalEnemy.Clone();
+
+            // Assert
+            Assert.NotSame(originalEnemy, clonedEnemy);
+        }
+
     }
 }
